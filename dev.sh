4441fcbcae38e8efdf9a1d22f133cd6c60fa8d19
Martin Aarhof <martin.aarhof@gmail.com>
2014-04-04 14:29:32 +0200

    changed minors

--- dev.sh
+++ dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 php app/console cache:clear --env=dev
+rm web/js/*
+rm web/css/*
 php app/console assets:install --env=dev --symlink
 php app/console assetic:dump --env=dev
 chmod 777 -R app/cache
--- prod.sh
+++ prod.sh
@@ -3,6 +3,8 @@ git pull
 npm install
 php composer.phar install
 php app/console cache:clear --env=prod
+rm web/js/*
+rm web/css/*
 php app/console assets:install --env=prod --symlink
 php app/console assetic:dump --env=prod
 chmod 777 -R app/cache
